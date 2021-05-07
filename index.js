19a2dd4a6cbbad08181cf446b5a23bcbd0bb57a1
Leonid Cupikov <dreambooms@gmail.com>
2021-05-07 10:06:21 +0300

    Update dependencies

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const request = require('request-promise');
-const whacko = require('whacko');
+const fetch = require('node-fetch');
+const HTMLParser = require('node-html-parser');
 const MONTHS = ['Janvaris', 'Februaris', 'Marts', 'Aprilis', 'Maijs', 'Junijs', 'Julijs', 'Augusts', 'Septembris', 'Oktobris', 'Novembris', 'Decembris'];
 
 const isHoliday = async date => {
-	const jsDate = new Date(date);
-	const body = await request(`https://xn--kalendrs-m7a.lv/${MONTHS[jsDate.getMonth()]}-${jsDate.getFullYear()}`);
-	const $ = whacko.load(body);
-	const dayItem = $($($('.item')[jsDate.getDate() - 1]).find('.dt'));
-	return dayItem.hasClass('dt-hd');
+	const response = await fetch(`https://xn--kalendrs-m7a.lv/${MONTHS[date.getMonth()]}-${date.getFullYear()}`);
+	const body = await response.text();
+	const document = HTMLParser.parse(body);
+	const dayItem = document.querySelectorAll('.item')[date.getDate() - 1];
+	return dayItem.querySelector('.dt').classList.contains('dt-hd');
 }
 
-const dateStr = '2018-04-30';
-const ih = isHoliday(dateStr);
-console.log(`${dateStr} - ${ih ? 'is' : 'not'} holiday`);
+(async () => {
+	const dateStr = new Date();
+	const ih = await isHoliday(dateStr);
+	console.log(`${dateStr} - ${ih ? 'is' : 'not'} holiday`);
+})();
