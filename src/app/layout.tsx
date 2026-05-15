c1475fcb353e5851e8b373c159ca7b39420b84ea
Leonid Cupikov <leonid.cupikov@gmail.com>
2026-05-15 09:20:05 +0200

    Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import { Analytics } from "@vercel/analytics/next";
-import clsx from "clsx";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
